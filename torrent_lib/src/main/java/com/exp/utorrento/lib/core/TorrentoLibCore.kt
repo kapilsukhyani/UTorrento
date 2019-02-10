73d765e18d87d522c9061208c074f39912dfc859
Kapil Sukhyani <ksukhyani@atlassian.com>
2019-02-10 11:19:08 -0800

    Adding method to load torrent file give a magent uri

--- torrent_lib/src/main/java/com/exp/utorrento/lib/core/TorrentoLibCore.kt
+++ torrent_lib/src/main/java/com/exp/utorrento/lib/core/TorrentoLibCore.kt
@@ -1,14 +1,18 @@
 package com.exp.utorrento.lib.core
 
 import bt.Bt
+import bt.StandaloneClientBuilder
 import bt.data.file.FileSystemStorage
 import bt.dht.DHTConfig
 import bt.dht.DHTModule
 import bt.magnet.MagnetUriParser
+import bt.metainfo.MetadataService
 import bt.metainfo.Torrent
 import bt.metainfo.TorrentId
+import bt.runtime.BtClient
 import bt.runtime.Config
 import bt.torrent.TorrentSessionState
+import bt.torrent.messaging.MetadataConsumer
 import com.exp.utorrento.extension.bt.startAsync
 import io.reactivex.Flowable
 import io.reactivex.Scheduler
@@ -31,7 +35,7 @@ interface TorrentoLibCore {
      * @param magnetUri, torrent magnet uri.
      * @param storageDir, directory to store downloaded content in.
      * @param period, dictates how far [TorrentDownloadState.Downloading] events should be from each other.
-     * @param observeOn, an optional scheduler to indicate to observe stream on
+     * @param observeOn, an optional scheduler to observe stream on, default is [Schedulers.computation]
      *
      * @return A stream of [TorrentDownloadState] events.
      */
@@ -42,8 +46,18 @@ interface TorrentoLibCore {
         observeOn: Scheduler = Schedulers.computation()
     ): Flowable<TorrentDownloadState>
 
+    /**
+     * Downloads torrent file for a give magnet url.
+     *
+     * @param magnetUri, torrent magnet uri.
+     * @param storageDir, directory to store downloaded content in.
+     * @param observeOn, an optional scheduler to observe result on, default is [Schedulers.computation]
+     *
+     * @return A [Single] of [TorrentDownloadState.TorrentFileLoaded]
+     */
     fun downloadTorrentForMagent(
         magnetUri: String,
+        storageDir: File,
         observeOn: Scheduler = Schedulers.computation()
     ): Single<TorrentDownloadState.TorrentFileLoaded>
 }
@@ -65,6 +79,22 @@ internal class TorrentoLibCoreDefaultImpl : TorrentoLibCore {
         })
     }
 
+    private fun getBtClientBuilder(
+        config: Config = defaultConfig(),
+        dhtModule: DHTModule = dhtModuleWithDefaultConfig(),
+        onTorrentFetched: ((Torrent) -> Unit)
+    ): StandaloneClientBuilder {
+        // create client with a private runtime
+        return Bt.client()
+            .afterFilesChosen {
+                println("Files selected")
+            }
+            .afterTorrentFetched(onTorrentFetched)
+            .config(config)
+            .autoLoadModules()
+            .module(dhtModule)
+    }
+
     override fun downloadMagnetContent(
         magnetUri: String,
         storageDir: File,
@@ -79,30 +109,23 @@ internal class TorrentoLibCoreDefaultImpl : TorrentoLibCore {
             val storage = FileSystemStorage(storageDir.toPath())
             val uri = MagnetUriParser.parser().parse(magnetUri)
             val torrentFileAvailableEventSource = PublishSubject.create<TorrentDownloadState.TorrentFileLoaded>()
+            val client = getBtClientBuilder { torrent ->
+                torrentFileAvailableEventSource.onNext(
+                    TorrentDownloadState.TorrentFileLoaded(
+                        torrent.torrentId,
+                        torrent
+                    )
+                )
+            }
+                .storage(storage)
+                .magnet(uri)
+                .stopWhenDownloaded()
+                .build()
 
-            // create client with a private runtime
             Triple(
                 uri.torrentId,
                 torrentFileAvailableEventSource,
-                Bt.client()
-                    .afterFilesChosen {
-                        println("Files selected")
-                    }
-                    .afterTorrentFetched { torrent ->
-                        torrentFileAvailableEventSource.onNext(
-                            TorrentDownloadState.TorrentFileLoaded(
-                                torrent.torrentId,
-                                torrent
-                            )
-                        )
-                    }
-                    .config(defaultConfig())
-                    .storage(storage)
-                    .magnet(uri)
-                    .autoLoadModules()
-                    .module(dhtModuleWithDefaultConfig())
-                    .stopWhenDownloaded()
-                    .build()
+                client
             )
         }
             .subscribeOn(Schedulers.io())
@@ -126,9 +149,38 @@ internal class TorrentoLibCoreDefaultImpl : TorrentoLibCore {
 
     override fun downloadTorrentForMagent(
         magnetUri: String,
+        storageDir: File,
         observeOn: Scheduler
     ): Single<TorrentDownloadState.TorrentFileLoaded> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Single.create<TorrentDownloadState.TorrentFileLoaded> { emitter ->
+            lateinit var client: BtClient
+            val uri = MagnetUriParser.parser().parse(magnetUri)
+            // create file system based backend for torrent data
+            val storage = FileSystemStorage(storageDir.toPath())
+
+            client = getBtClientBuilder { torrent ->
+                if (!emitter.isDisposed) {
+                    emitter.onSuccess(
+                        TorrentDownloadState.TorrentFileLoaded(
+                            torrent.torrentId,
+                            torrent
+                        )
+                    )
+                }
+                client.stop()
+            }
+                .storage(storage)
+                .magnet(uri)
+                .build()
+
+            emitter.setCancellable {
+                client.stop()
+            }
+
+            client.startAsync()
+        }
+            .observeOn(observeOn)
+
     }
 
 }
@@ -188,7 +240,7 @@ sealed class TorrentDownloadState {
 }
 
 
-fun initCore() {
+fun downloadMagnetContentExample() {
     val targetDirectory = File("/Users/ksukhyani/Downloads")
     val core = TorrentoLibCore()
     val disposable = core.downloadMagnetContent(
@@ -205,6 +257,35 @@ fun initCore() {
     disposable.dispose()
 }
 
+fun downloadTorrentForMagnetExample() {
+    val targetDirectory = File("/Users/ksukhyani/Downloads")
+
+    val core = TorrentoLibCore()
+    val disposable = core.downloadTorrentForMagent(
+        "magnet:?xt=urn:btih:75dc10d7f281351f78a02843d15d737e7fc4d59a&dn=BigSean&tr=udp://thetracker.org:80/announce&tr=udp://tracker.cyberia.is:6969/announce&tr=udp://open.demonii.si:1337/announce&tr=udp://denis.stalker.upeer.me:6969/announce&tr=udp://tracker.port443.xyz:6969/announce&tr=udp://torrentclub.tech:6969/announce&tr=udp://tracker.filemail.com:6969/announce&tr=udp://tracker.filepit.to:6969/announce&tr=udp://tracker.moeking.me:6969/announce&tr=udp://retracker.netbynet.ru:2710/announce&tr=udp://00.syo.mx:53/announce&tr=https://1.tracker.eu.org:443/announce&tr=https://2.tracker.eu.org:443/announce&tr=udp://newtoncity.org:6969/announce&tr=udp://tracker.novg.net:6969/announce&tr=https://3.tracker.eu.org:443/announce&tr=udp://bt.oiyo.tk:6969/announce&tr=udp://retracker.lanta-net.ru:2710/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://tracker.birkenwald.de:6969/announce&tr=udp://chihaya.toss.li:9696/announce&tr=udp://tracker.skynetcloud.tk:6969/announce&tr=udp://tracker.beeimg.com:6969/announce&tr=http://tracker2.dler.org:80/announce&tr=udp://tracker.tiny-vps.com:6969/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.uw0.xyz:6969/announce&tr=udp://tracker.nyaa.uk:6969/announce&tr=http://vps02.net.orel.ru:80/announce&tr=http://tracker.openzim.org:80/announce&tr=http://tracker.bz:80/announce&tr=udp://retracker.hotplug.ru:2710/announce&tr=udp://tracker.dyn.im:6969/announce&tr=udp://explodie.org:6969/announce&tr=udp://tracker4.itzmx.com:2710/announce&tr=http://t.acg.rip:6699/announce&tr=udp://tracker.dler.org:6969/announce&tr=http://t.nyaatracker.com:80/announce&tr=udp://bt.xxx-tracker.com:2710/announce&tr=udp://ipv4.tracker.harry.lu:80/announce&tr=udp://bigfoot1942.sektori.org:6969/announce&tr=udp://hk1.opentracker.ga:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce&tr=http://torrent.nwps.ws:80/announce&tr=http://tracker.gbitt.info:80/announce&tr=https://1337.abcvg.info:443/announce&tr=udp://open.stealth.si:80/announce&tr=http://tracker3.itzmx.com:6961/announce&tr=http://tracker.corpscorp.online:80/announce&tr=udp://tracker.freddit.nl:6969/announce&tr=udp://tracker.iamhansen.xyz:2000/announce&tr=udp://tracker.leechers-paradise.org:6969/announce&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.com:2710/announce&tr=udp://tracker2.itzmx.com:6961/announce&tr=udp://tracker1.itzmx.com:8080/announce&tr=http://open.trackerlist.xyz:80/announce&tr=https://opentracker.xyz:443/announce&tr=https://tracker.fastdownload.xyz:443/announce&tr=https://t.quic.ws:443/announce&tr=udp://retracker.sevstar.net:2710/announce&tr=http://tracker6.emce.org:12345/announce&tr=udp://tracker.open-tracker.org:1337/announce&tr=http://tracker.torrentyorg.pl:80/announce&tr=udp://ipv6.tracker.harry.lu:80/announce&tr=http://tracker4.emce.org:12345/announce&tr=udp://tracker.swateam.org.uk:2710/announce",
+        targetDirectory
+    )
+        .subscribe({
+            println(it)
+        }, {
+            println("error $it")
+        })
+    System.`in`.read()
+    disposable.dispose()
+}
+
+fun downloadTorrentForMagnetWithoutClientExample() {
+    //this solution isn't working, need to figure out what is missing
+    val magnet = MagnetUriParser.parser()
+        .parse("magnet:?xt=urn:btih:75dc10d7f281351f78a02843d15d737e7fc4d59a&dn=BigSean&tr=udp://thetracker.org:80/announce&tr=udp://tracker.cyberia.is:6969/announce&tr=udp://open.demonii.si:1337/announce&tr=udp://denis.stalker.upeer.me:6969/announce&tr=udp://tracker.port443.xyz:6969/announce&tr=udp://torrentclub.tech:6969/announce&tr=udp://tracker.filemail.com:6969/announce&tr=udp://tracker.filepit.to:6969/announce&tr=udp://tracker.moeking.me:6969/announce&tr=udp://retracker.netbynet.ru:2710/announce&tr=udp://00.syo.mx:53/announce&tr=https://1.tracker.eu.org:443/announce&tr=https://2.tracker.eu.org:443/announce&tr=udp://newtoncity.org:6969/announce&tr=udp://tracker.novg.net:6969/announce&tr=https://3.tracker.eu.org:443/announce&tr=udp://bt.oiyo.tk:6969/announce&tr=udp://retracker.lanta-net.ru:2710/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://tracker.birkenwald.de:6969/announce&tr=udp://chihaya.toss.li:9696/announce&tr=udp://tracker.skynetcloud.tk:6969/announce&tr=udp://tracker.beeimg.com:6969/announce&tr=http://tracker2.dler.org:80/announce&tr=udp://tracker.tiny-vps.com:6969/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.uw0.xyz:6969/announce&tr=udp://tracker.nyaa.uk:6969/announce&tr=http://vps02.net.orel.ru:80/announce&tr=http://tracker.openzim.org:80/announce&tr=http://tracker.bz:80/announce&tr=udp://retracker.hotplug.ru:2710/announce&tr=udp://tracker.dyn.im:6969/announce&tr=udp://explodie.org:6969/announce&tr=udp://tracker4.itzmx.com:2710/announce&tr=http://t.acg.rip:6699/announce&tr=udp://tracker.dler.org:6969/announce&tr=http://t.nyaatracker.com:80/announce&tr=udp://bt.xxx-tracker.com:2710/announce&tr=udp://ipv4.tracker.harry.lu:80/announce&tr=udp://bigfoot1942.sektori.org:6969/announce&tr=udp://hk1.opentracker.ga:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.internetwarriors.net:1337/announce&tr=http://torrent.nwps.ws:80/announce&tr=http://tracker.gbitt.info:80/announce&tr=https://1337.abcvg.info:443/announce&tr=udp://open.stealth.si:80/announce&tr=http://tracker3.itzmx.com:6961/announce&tr=http://tracker.corpscorp.online:80/announce&tr=udp://tracker.freddit.nl:6969/announce&tr=udp://tracker.iamhansen.xyz:2000/announce&tr=udp://tracker.leechers-paradise.org:6969/announce&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://9.rarbg.com:2710/announce&tr=udp://tracker2.itzmx.com:6961/announce&tr=udp://tracker1.itzmx.com:8080/announce&tr=http://open.trackerlist.xyz:80/announce&tr=https://opentracker.xyz:443/announce&tr=https://tracker.fastdownload.xyz:443/announce&tr=https://t.quic.ws:443/announce&tr=udp://retracker.sevstar.net:2710/announce&tr=http://tracker6.emce.org:12345/announce&tr=udp://tracker.open-tracker.org:1337/announce&tr=http://tracker.torrentyorg.pl:80/announce&tr=udp://ipv6.tracker.harry.lu:80/announce&tr=http://tracker4.emce.org:12345/announce&tr=udp://tracker.swateam.org.uk:2710/announce")
+    val service = MetadataService()
+    val metadataConsumer = MetadataConsumer(service, magnet.torrentId, Config())
+    val torrent = metadataConsumer.waitForTorrent()
+    print(TorrentDownloadState.TorrentFileLoaded(torrent.torrentId, torrent))
+}
+
 fun main(args: Array<String>) {
-    initCore()
+//    downloadMagnetContentExample()
+    downloadTorrentForMagnetExample()
+//    downloadTorrentForMagnetWithoutClientExample()
 }
